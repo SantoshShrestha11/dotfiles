cc7649656523c684b4078821d8f38124a7a9a8c4
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-11 18:02:05 +0545

    remapped the things

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -70,11 +70,12 @@ vim.keymap.set('v', 'p', '"_dP', opts)
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', function()
-  vim.diagnostic.jump { count = -1, float = true, { desc = 'Go to previous diagnostic message' } }
-end)
+  vim.diagnostic.jump { count = -1, float = true }
+end, { desc = 'Go to previous diagnostic message' })
+
 vim.keymap.set('n', ']d', function()
-  vim.diagnostic.jump { count = 1, float = true, { desc = 'Go to next diagnostic message' } }
-end)
+  vim.diagnostic.jump { count = 1, float = true }
+end, { desc = 'Go to next diagnostic message' })
 
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
