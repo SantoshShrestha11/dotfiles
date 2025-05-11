c9671441581805aa1fdd71b1c6c03b54e87e21fa
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-11 20:57:16 +0545

    lol done with the config of the color of the yaniking

--- nvim/.config/nvim/lua/core/snippets.lua
+++ nvim/.config/nvim/lua/core/snippets.lua
@@ -35,7 +35,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.hl.on_yank {
       higroup = 'YankHighlight', -- Use your custom highlight group
-      timeout = 200, -- Duration in milliseconds
+      timeout = 100, -- Duration in milliseconds
     }
   end,
   group = highlight_group,
