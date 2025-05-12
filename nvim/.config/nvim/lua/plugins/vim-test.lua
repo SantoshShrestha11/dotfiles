0db3c88127660ebf36e9069adaba2681afc54dd8
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-12 15:33:35 +0545

    added the vim test

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,6 +1,7 @@
 return {
   'vim-test/vim-test',
   dependencies = {
+    enabled = true,
     'preservim/vimux',
   },
   config = function()
@@ -8,7 +9,7 @@ return {
     vim.keymap.set('n', '<leader>T', ':TestFile<CR>', {})
     vim.keymap.set('n', '<leader>a', ':TestSuite<CR>', {})
     vim.keymap.set('n', '<leader>l', ':TestLast<CR>', {})
-    vim.keymap.set('n', '<leader>g', ':TestVisit<CR>', {})
+    -- vim.keymap.set('n', '<leader>g', ':TestVisit<CR>', {})
     vim.cmd "let test#strategy = 'vimux'"
   end,
 }
