4a428732aadc77401c1c23540c1e2de61fd9657d
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-01 00:57:31 +0545

    keymap for the  telescope harpoon picker

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -51,6 +51,33 @@ return {
         desc = 'Harpoon to File ' .. i,
       })
     end
+
+    local harpoon = require 'harpoon'
+    harpoon:setup {}
+
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table {
+            results = file_paths,
+          },
+          previewer = conf.file_previewer {},
+          sorter = conf.generic_sorter {},
+        })
+        :find()
+    end
+
+    vim.keymap.set('n', '<C-p>', function()
+      toggle_telescope(harpoon:list())
+    end, { desc = 'Open Harpoon Telescope Picker' })
+
     return keys
   end,
 }
