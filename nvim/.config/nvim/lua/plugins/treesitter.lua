dd038302c0aac927f2c96b942a84862695fd9c6c
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-04 23:41:25 +0545

    sync

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -33,7 +33,7 @@ return { -- Highlight, edit, and navigate code
       'html',
       'rust',
       'ron',
-      'php',
+      -- 'php',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
@@ -113,7 +113,7 @@ return { -- Highlight, edit, and navigate code
           -- mapping query_strings to modes.
           selection_modes = {
             ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V',  -- linewise
+            ['@function.outer'] = 'V', -- linewise
             ['@class.outer'] = '<c-v>', -- blockwise
           },
           -- If you set this to `true` (default is `false`) then any textobject is
